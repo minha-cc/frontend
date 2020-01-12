082bfd902ab2d0b92d58204ed65240230b0d2cf1
Marco Rojo <mrmarcondes@gmail.com>
2020-01-11 23:12:13 -0300

    fix firebase plugin

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,9 +2,9 @@ import * as firebase from 'firebase/app'
 import 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: process.env.vue.firebase_api_key,
-  projectId: process.env.vue.firebase_project_id,
-  authDomain: `${process.env.vue.firebase_project_id}.firebaseapp.com`
+  apiKey: process.env.firebase_api_key,
+  projectId: process.env.firebase_project_id,
+  authDomain: `${process.env.firebase_project_id}.firebaseapp.com`
 }
 
 firebase.apps.length ? firebase.app() : firebase.initializeApp(firebaseConfig)
