463ba792e8a70f1668aeed6a9a1678c6c102c53b
Marco Rojo <mrmarcondes@gmail.com>
2020-01-22 22:49:39 -0300

    add signup component

---
+++ components/Auth/Signup.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-content>
+    <v-container>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="12" md="12">
+          <v-form @submit.prevent="onSignIn" class="mt-2">
+            <v-card-text></v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn :loading="loading" type="submit" color="primary">
+                Cadastrar
+              </v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {}
+</script>
