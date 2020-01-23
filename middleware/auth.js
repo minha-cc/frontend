61c401244d10f7691e8cf5c628708eae517fe0af
Marco Rojo <mrmarcondes@gmail.com>
2020-01-22 23:18:13 -0300

    add constant to root path

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,17 @@
+import { AUTH_PATH } from '@/MinhaCCConstants'
+
 export default function({ store, redirect, route }) {
   const firebaseUid = localStorage.getItem('firebaseUid')
-  if (firebaseUid != null && route.name === 'signin') {
+  if (firebaseUid != null && route.name === AUTH_PATH) {
     redirect('/dashboard')
   }
   if (firebaseUid == null && isInternalRoute(route)) {
-    redirect('/signin')
+    redirect(`/${AUTH_PATH}`)
   }
 }
 
 function isInternalRoute(route) {
-  if (route.matched.some((record) => record.path !== '/signin')) {
+  if (route.matched.some((record) => record.path !== `/${AUTH_PATH}`)) {
     return true
   }
 }
