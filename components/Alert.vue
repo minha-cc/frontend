c84ab6b52e0b535bc89023367d8b01097458e323
Marco Rojo <mrmarcondes@gmail.com>
2020-01-22 22:28:12 -0300

    add components

---
+++ components/Alert.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-content>
+    <v-container>
+      <v-alert
+        v-model="hasMessage"
+        :color="color"
+        transition="fade-transition"
+        text
+        outlined
+        border="left"
+        dismissible
+      >
+        {{ message }}
+      </v-alert>
+    </v-container>
+  </v-content>
+</template>
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: null
+    },
+    color: {
+      type: String,
+      default: 'error'
+    }
+  },
+  data() {
+    return {
+      hasMessage: false
+    }
+  },
+  watch: {
+    message(value) {
+      this.hasMessage = value !== null
+    }
+  }
+}
+</script>
---
+++ pages/auth/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app id="auth">
+    <v-content>
+      <v-container>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="6">
+            <v-layout justify-center>
+              <v-img
+                :src="require('~/assets/images/minha-cc-logo.png')"
+                aspect-ratio="1"
+                max-width="238"
+                max-height="87"
+              />
+            </v-layout>
+            <Alert :message="message" />
+            <v-card outlined>
+              <v-tabs centered>
+                <v-tab>
+                  Entrar
+                </v-tab>
+                <v-tab>
+                  Cadastrar
+                </v-tab>
+              </v-tabs>
+              <Signin @onError="message = $event" />
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+<script>
+import Signin from '@/components/Auth/Signin.vue'
+import Alert from '@/components/Alert.vue'
+export default {
+  components: {
+    Signin,
+    Alert
+  },
+  data() {
+    return {
+      message: null
+    }
+  }
+}
+</script>
