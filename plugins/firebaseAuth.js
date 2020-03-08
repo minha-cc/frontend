253a3ec83d730ba84158855256e65b6ac7083401
Marco Rojo <mrmarcondes@gmail.com>
2020-03-07 21:14:01 -0300

    try to fix issue

--- plugins/firebaseAuth.js
+++ plugins/firebaseAuth.js
@@ -1,4 +1,4 @@
-import { firebaseAuth, db } from '@/plugins/firebase'
+import { firebaseAuth } from '@/plugins/firebase'
 
 export default (context) => {
   return new Promise((resolve, reject) => {
@@ -6,26 +6,16 @@ export default (context) => {
       if (!user) {
         return resolve()
       }
-      const accounts = db.collection('accounts')
-      accounts
-        .doc(user.uid)
-        .get()
-        .then((doc) => {
-          if (doc.exists) {
-            const accountId = doc.data()
-            const loggedInUser = {
-              uid: user.uid,
-              username: user.displayName,
-              email: user.email,
-              accountId
-            }
-            return resolve(
-              context.store.dispatch('auth/setCurrentUser', loggedInUser)
-            )
-          } else {
-            return resolve()
-          }
-        })
+
+      const loggedInUser = {
+        uid: user.uid,
+        username: user.displayName,
+        email: user.email
+      }
+
+      return resolve(
+        context.store.dispatch('auth/setCurrentUser', loggedInUser)
+      )
     })
   })
 }
