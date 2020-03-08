96d5d8af637d2ff6d1bad65fe67349d41a139f89
Marco Rojo <mrmarcondes@gmail.com>
2020-03-07 21:21:31 -0300

    remove accountId from store (#27)

--- plugins/firebaseAuth.js
+++ plugins/firebaseAuth.js
@@ -1,32 +1,21 @@
-import { firebaseAuth, db } from '@/plugins/firebase'
+import { firebaseAuth } from '@/plugins/firebase'
 
 export default (context) => {
-  const accounts = db.collection('accounts')
-
   return new Promise((resolve, reject) => {
     firebaseAuth.onAuthStateChanged((user) => {
       if (!user) {
         return resolve()
       }
-      accounts
-        .doc(user.uid)
-        .get()
-        .then((doc) => {
-          if (doc.exists) {
-            const accountId = doc.data()
-            const loggedInUser = {
-              uid: user.uid,
-              username: user.displayName,
-              email: user.email,
-              accountId
-            }
-            return resolve(
-              context.store.dispatch('auth/setCurrentUser', loggedInUser)
-            )
-          } else {
-            return resolve()
-          }
-        })
+
+      const loggedInUser = {
+        uid: user.uid,
+        username: user.displayName,
+        email: user.email
+      }
+
+      return resolve(
+        context.store.dispatch('auth/setCurrentUser', loggedInUser)
+      )
     })
   })
 }
